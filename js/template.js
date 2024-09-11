04fa6119f1c98e3d0e8d564518948457497c2b0c
pyglauce <glauceagc@hotmail.com>
2024-09-11 15:34:09 -0300

    meu salão

--- js/template.js
+++ js/template.js
@@ -1,24 +1,56 @@
+$(document).ready(function() {
+    // Inicializa a aplicação quando o documento estiver pronto
+    runApp();
+});
+
 function runApp() {
-    $('#wrap>nav').hide();
-    $('#fumaca').hide();
-    $('#toggleMenu').click(controlaMenu)
+    // Ajusta o menu ao carregar a página
+    resizeMenu();
+
+    // Ajusta o menu ao redimensionar a janela
+    $(window).resize(resizeMenu);
+
+    // Alterna a visibilidade do menu ao clicar no botão
+    $('#toggleMenu').click(toggleMenu);
 }
 
-function controlaMenu() {
+function resizeMenu() {
+    // Mostra ou esconde o menu com base na largura da janela
+    if (window.innerWidth >= 640) {
+        showMenu(true);
+    } else {
+        hideMenu();
+    }
+}
 
+function toggleMenu() {
+    // Alterna a visibilidade do menu ao clicar no botão
     if ($('#wrap>nav').is(':visible')) {
-        $('#wrap>nav').hide('fast');
-        $('#fumaca').hide('fast');
+        hideMenu();
+    } else {
+        showMenu();
+    }
+    return false; // Previne o comportamento padrão do clique
+}
+
+function showMenu(noSmoke) {
+    // Mostra o menu
+    $('#wrap>nav').show('fast');
+
+    if (noSmoke) {
+        // Esconde o fundo escuro e permite rolagem
+        $('#menuSmoke').hide('fast');
         $('body').css('overflow', 'auto');
     } else {
-        $('#wrap>nav').show('fast');
-        $('#fumaca').show('fast');
+        // Mostra o fundo escuro e desabilita rolagem
+        $('#menuSmoke').show('fast');
         $('body').css('overflow', 'hidden');
     }
-
-    return false;
 }
 
-
-
-$(document).ready(runApp);
+function hideMenu() {
+    // Esconde o menu e o fundo escuro, permite rolagem
+    $('#wrap>nav').hide('fast');
+    $('#menuSmoke').hide('fast');
+    $('body').css('overflow', 'auto');
+}
